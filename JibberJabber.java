187de157a2f8abfde702e0f7fbbafc012a5d22b2
tofr <mykolyk@stuycs.org>
2014-05-22 09:06:41 -0400

    Mr. T weighs in

---
+++ JibberJabber.java
@@ -0,0 +1,13 @@
+public class JibberJabber {
+
+    public static void main( String[] args ) {
+
+	System.out.println("Quit yo jibber jabber!");
+	System.out.println("... A new team has appeared! Firenze and EmmaL on the scene --- ");	
+	System.out.println("...");
+	System.out.println("...");
+	System.out.println("...");
+	System.out.println("I pity tha foo.");
+
+    }
+}
