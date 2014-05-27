ce2f1db12ddb31405d56357bbc9b751589626279
Florence Lo <florence12897@yahoo.com>
2014-05-26 23:59:53 -0400

    Emma. Rate me.

---
+++ FlorenceLo.java
@@ -0,0 +1,10 @@
+
+public class FlorenceLo extends Students{
+
+	public FlorenceLo() {
+		super("Florence",
+				"Also known as Flolo or Firenze, her friends brighten up her day and her hard work never ends.",
+				wisdom, likability, Enthusiasm, Charisma, Bravery, Grades);
+	}
+
+}
