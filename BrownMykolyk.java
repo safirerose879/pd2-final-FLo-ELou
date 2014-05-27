757d7d1cc897f2c94297f3defe9775af66c04740
Florence Lo <florence12897@yahoo.com>
2014-05-26 21:05:58 -0400

    updates

---
+++ BrownMykolyk.java
@@ -0,0 +1,11 @@
+
+public class BrownMykolyk extends Teachers{
+
+	public BrownMykolyk() {
+		super("Mr.BrownMykolyk",
+				"A rad teacher who dabble in the arts of Computer Science magic and the world of Geometry. His favorite color: Math.",
+				13, 12, 15, 10, 12, 15);
+	}
+
+	
+}
---
+++ Zamansky.java
@@ -0,0 +1,10 @@
+
+public class Zamansky extends Teachers{
+
+	public Zamansky() {
+		super("Mr.Zamansky",
+				"A respected and valuable asset to the Computer Science community as well as an awesome teacher.",
+				13, 11, 12, 12, 14, 15);
+	}
+
+}
