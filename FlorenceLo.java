96190d37a60fa8f1d243351067b65324c0ec9798
Emma Lou <louemma2@gmail.com>
2014-05-28 08:50:34 -0400

    I did it!

--- FlorenceLo.java
+++ FlorenceLo.java
@@ -4,7 +4,7 @@ public class FlorenceLo extends Students{
 	public FlorenceLo() {
 		super("Florence",
 				"Also known as Flolo or Firenze, her friends brighten up her day and her hard work never ends.",
-				wisdom, likability, Enthusiasm, Charisma, Bravery, Grades);
+				11, 10, 12, 10, 8, 12 );
 	}
 
 }
