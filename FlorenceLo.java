1056e21be8bc5d3e0ef764a216e256f3575bbd50
Florence Lo <xin.lo@cslab1-19.(none)>
2014-05-28 08:52:24 -0400

    sheesh

--- FlorenceLo.java
+++ FlorenceLo.java
@@ -4,7 +4,7 @@ public class FlorenceLo extends Students{
 	public FlorenceLo() {
 		super("Florence",
 				"Also known as Flolo or Firenze, her friends brighten up her day and her hard work never ends.",
-				11, 10, 12, 10, 8, 12 );
+				11, 10, 12, 10, 8, 95);
 	}
 
 }
