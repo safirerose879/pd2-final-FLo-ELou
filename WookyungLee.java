e27ed089ca373171057df51bfb788731a8f68e00
Florence Lo <xin.lo@cslab1-19.(none)>
2014-06-03 09:01:07 -0400

    WOOOOKKKKYYYYYYY

---
+++ WookyungLee.java
@@ -0,0 +1,9 @@
+public class WookyungLee extends Students{
+
+	public WookyungLee() {
+		super("Wookyung","Commonly called as Wooky, a highly intelligent being who pwns in Japanese.",
+				14, 13, 10, 14, 9, 97);
+	}
+
+}
+
