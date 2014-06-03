3a7bb35255ffd3b97d4ddafb27f168ec7bf7c1a5
Florence Lo <xin.lo@cslab1-19.(none)>
2014-06-03 09:09:20 -0400

    updates

---
+++ AngelaLin.java
@@ -0,0 +1,9 @@
+public class AngelaLin extends Students{
+
+	public AngelaLin() {
+	    super("Angela","A lovable child who loves showing affection except in front of teachers.",
+				13, 14, 13, 12, 8, 93);
+	}
+
+}
+
--- WookyungLee.java
+++ WookyungLee.java
@@ -1,7 +1,7 @@
 public class WookyungLee extends Students{
 
 	public WookyungLee() {
-		super("Wookyung","Commonly called as Wooky, a highly intelligent being who pwns in Japanese.",
+		super("Wookyung","Commonly called as Wooky, a highly intelligent being who pwns in Japanese. Also an amazing dancer who needs to stop doing homework during lunch.",
 				14, 13, 10, 14, 9, 97);
 	}
 
