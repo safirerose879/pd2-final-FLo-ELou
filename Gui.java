b64afb1cdbd92910dc8ff6be53464663656e03a1
Emma Lou <louemma2@gmail.com>
2014-06-03 09:17:03 -0400

    Gui, my worst nightmare

---
+++ Gui.java
@@ -0,0 +1,145 @@
+import java.util.*;
+import java.io.*;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
+
+public class Gui extends JFrame implements ActionListener {
+    
+    private Container buttons;
+    private JFrame pane;
+    private JButton exitButton, wisdom, likability, enthusiasm, charisma, bravery;
+    private JLabel heading, obstacle; 
+    private JPanel panel;
+    private JTextArea ta;
+    private ButtonGroup radioButtons;
+  
+
+
+    private class myKeyListener implements KeyListener {
+	public void keyPressed(KeyEvent e) {
+	}
+	public void keyReleased(KeyEvent e) {
+	}
+	public void keyTyped(KeyEvent e) {  
+	}
+    }
+     private class TextAreaOutputStream extends OutputStream {
+	 private final JTextArea textArea;
+	 private final StringBuilder sb = new StringBuilder();
+
+	public TextAreaOutputStream(final JTextArea textArea){
+	    this.textArea = textArea;
+	}
+
+	public void flush(){
+	}
+	public void close(){
+	}
+	public void write (int b) throws IOException {
+	    if (b == '\r'){
+		return;
+	    }
+	    if (b == '\n') {
+		final String text = sb.toString() + "\n";
+		textArea.append(text);
+		sb.setLength(0);
+	    }
+	    else {
+		sb.append((char)b);
+	    }
+	}
+
+     }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == exitButton) {
+            System.exit(0);
+        } 
+    }
+
+    
+
+    protected ImageIcon createImageIcon(String path,
+					String description) {
+	java.net.URL imgURL = getClass().getResource(path);
+	if (imgURL != null) {
+	    return new ImageIcon(imgURL, description);
+	} else {
+	    System.err.println("Couldn't find file: " + path);
+	    return null;
+	}
+    }
+
+
+    public Gui(){
+	pane = new JFrame();
+	//wisdom, likability, enthusiasm, charisma, bravery;
+       
+	pane.getContentPane().setLayout(new FlowLayout());
+	pane.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	exitButton = new JButton("QUIT");
+	wisdom = new JButton("WISDOM");
+	likability = new JButton("LIKABILITY");
+	enthusiasm = new JButton("ENTHUSIASM");
+	charisma = new JButton("CHARISMA");
+	bravery = new JButton("BRAVERY");
+	heading = new JLabel ("CARD GAME!");
+
+	ta = new JTextArea(450, 20);
+	ta.setEditable(false);
+	System.setOut(new PrintStream(new TextAreaOutputStream(ta)));
+
+
+	radioButtons = new ButtonGroup();
+
+	//makes track in very beginning because 25meters is selected as true first
+
+	buttons = new Container();
+	buttons.setLayout(new FlowLayout());
+  
+        buttons.add(exitButton);
+        buttons.add(wisdom);
+        buttons.add(likability);
+        buttons.add(enthusiasm);
+        buttons.add(charisma);
+        buttons.add(bravery);
+        buttons.add(heading);
+	//buttons.add(obstacle); //for testing purposes
+
+	heading.setFont(new Font("Broadway", Font.BOLD, 20));
+
+	panel = new JPanel();
+	panel.add(buttons);
+
+	pane.add(panel);
+
+	//pane.add(new JScrollPane(ta));
+	JScrollPane sp = new JScrollPane(ta); 
+	pane.add(sp);
+	pane.pack();
+	pane.setVisible(true);	
+
+	exitButton.addActionListener(this); 
+        wisdom.addActionListener(this);
+	likability.addActionListener(this);
+	enthusiasm.addActionListener(this);
+	charisma.addActionListener(this);
+	bravery.addActionListener(this);
+
+	pane.setTitle("Kirby Minigame");
+	//panel.setBorder(BorderFactory.createLineBorder(Color.blue, 3));
+	pane.setSize(750,400);
+	pane.setLocation(250,150);	
+	pane.setVisible(true); 
+    }
+
+ public static void main(String[] args) {
+        Gui g = new Gui();
+	//TextAreaOutputStream taos = new TextAreaOutputStream (ta);
+    }
+}
