b3f85c0e9049e61d5ea09fe969d3b5e4b5db03b6
Florence Lo <xin.lo@cslab1-19.(none)>
2014-06-04 09:18:39 -0400

    PULL ME EMMMAAAAAA hehehe

---
+++ Bob.java
@@ -0,0 +1,9 @@
+
+public class Bob extends Students{
+
+	public Bob() {
+		super("Bob","A random fictional character that is common in all decks.",
+		      7, 6, 3, 2, 5, 50);
+	}
+
+}
---
+++ Dave.java
@@ -0,0 +1,9 @@
+
+public class Dave extends Students{
+
+	public Dave() {
+		super("Dave","A random fictional character that is common in all decks.",
+		      6, 8, 1, 4, 7, 92);
+	}
+
+}
