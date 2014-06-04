54669f39047afeabb78c455f2d5cf95b28df21c0
Florence Lo <florence12897@yahoo.com>
2014-06-03 21:30:42 -0400

    update

---
+++ LawrenceLim.java
@@ -0,0 +1,9 @@
+public class LawrenceLim extends Students{
+
+	public LawrenceLim() {
+		super("Lawrence","Usually transforms into a cat, a helpful hugger. A great computer scientist.",
+				14, 13, 7, 8, 9, 94);
+	}
+
+}
+
