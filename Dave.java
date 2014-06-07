dd93f7e9d2f826c821899981051fdc4073efe132
Emma Lou <louemma2@gmail.com>
2014-06-07 15:59:18 -0400

    Update Dave.java

--- Dave.java
+++ Dave.java
@@ -2,7 +2,7 @@
 public class Dave extends Students{
 
 	public Dave() {
-		super("Dave","A random fictional character that is common in all decks.",
+		super("Dave","A random fictional character that is common in all decks. He's also the most adorable minion you'll ever find.",
 		      6, 8, 1, 4, 7, 92);
 	}
 
