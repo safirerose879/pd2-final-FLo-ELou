9064bbd8004715859823c54e2f17a876b8b2e8d9
Florence Lo <florence12897@yahoo.com>
2014-06-08 20:21:07 -0400

    not sure how to fix it so i have two. this edition has a nullpointererror.

---
+++ Deck2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Deck2 extends ArrayList<Cards> {
+
+	ArrayList<Cards> Starter, BoringDeck;
+	
+	public Deck2(){
+		
+		Bob b = new Bob();
+		Dave d = new Dave();
+
+		System.out.println("Checking");
+		System.out.println(b.getName());
+
+		BoringDeck.add(b);
+		BoringDeck.add(b);
+		BoringDeck.add(b);
+		BoringDeck.add(b);
+		BoringDeck.add(b);
+		BoringDeck.add(d);
+		BoringDeck.add(d);
+		BoringDeck.add(d);
+		BoringDeck.add(d);
+		BoringDeck.add(d);
+		
+		System.out.println("hmmm");
+
+		while(BoringDeck.size() > 0){
+			int num = (int) (Math.random() * BoringDeck.size() - 1);
+			Starter.add(BoringDeck.get(num));
+			BoringDeck.remove(num);
+		}
+	}
+	
+	public String ShowDeck(){
+		String str = "";
+		for(int i = 0;i<Starter.size() ;i++){
+			str += Starter.get(i).getName() + "\n";
+		}
+		return str;
+	}
+	
+	public static void main(String args[]){
+		Deck2 a = new Deck2();
+		System.out.println(a.ShowDeck());
+		
+	}
+}
