fdca8ade324e48b25a131b500a51f8c823dca9fb
Emma Lou <louemma2@gmail.com>
2014-06-08 20:35:58 -0400

    Finally updated description c;

--- EmmaLou.java
+++ EmmaLou.java
@@ -2,7 +2,7 @@
 public class EmmaLou extends Students{
 
 	public EmmaLou() {
-		super("Emma","",
+		super("Emma","A girl whose only wish is for people to pronounce her last name correctly.",
 				9, 10, 12, 12, 8, 92);
 	}
 
