a6f5cf3ab232d997fb3e98360a572f9252aec874
Emma Lou <louemma2@gmail.com>
2014-06-10 09:16:59 -0400

    YAY PROGRESSZZ

--- PlayGame.java
+++ PlayGame.java
@@ -3,9 +3,15 @@ import java.util.*;
 
 public class PlayGame{
 
-    private ArrayList<Cards>specialCards, normal, myDeck;
+    private ArrayList<Cards>specialCards, normal, myDeck, everything;
 
     public PlayGame(){
+
+	specialCards = new ArrayList<Cards>();
+	normal = new ArrayList<Cards>();
+	myDeck = new ArrayList<Cards>();
+	everything = new ArrayList<Cards>();
+
 	Bob b = new Bob();
 	Dave d = new Dave();
 
@@ -26,11 +32,44 @@ public class PlayGame{
 	    normal.add(d);
 	}
 
+	specialCards.add(AL);
+	specialCards.add(BM);
+	specialCards.add(CO);
+	specialCards.add(EL);
+	specialCards.add(FL);
+	specialCards.add(JD);
+	specialCards.add(KK);
+	specialCards.add(LL);
+	specialCards.add(MC);
+	specialCards.add(WL);
+	specialCards.add(ZA);
+
+	for (int i = 0; i < normal.size(); i++){
+	    everything.add(normal.get(i));
+	}
+	for (int i = 0; i < specialCards.size(); i++){
+	    everything.add(specialCards.get(i));
+	}
 
     }
 
-    public void makeDeck(){
+    public String makeDeck(){
 
+	Random r = new Random();
+	for (int i = 0; i < 10; i++){
+	    int n = r.nextInt(everything.size());
+	    myDeck.add(everything.get(n));
+	}
+	String s = new String();
+	for (int i = 0; i < myDeck.size(); i++){
+	    s +=myDeck.get(i).getName() + "\n";
+	}
+	return s;
+    }
+
+    public static void main (String args[]){
+	PlayGame a = new PlayGame();
+	System.out.println(a.makeDeck());
     }
 
 
